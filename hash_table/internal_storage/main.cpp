416a88bf35740ca2e9e9861aec21e9341ab9c20e
LuoHuaqing <luohuaqing@4paradigm.com>
2016-07-19 14:23:49 +0800

    fix

--- hash_table/internal_storage/main.cpp
+++ hash_table/internal_storage/main.cpp
@@ -2,26 +2,38 @@
 #include <vector>
 #include <assert.h>
 
-#include <ctime>
-
 #include "immutable_hash_table.h"
 
 using namespace std;
 using namespace paradigm4;
 
-int main() {
-    vector<immutable_hash_table<int, int>::entryT> vec =
-            { {1, 1}, {2, 2}, {3, 3}, {4, 4}, {33, 33}, {44, 44,}, {199999, 199999} };
-    auto backup = vec;
-    immutable_hash_table<int, int> ht(vec);
+int test() {
+    const int n = 10000;
+    srand((unsigned)time(NULL));
+    vector<std::pair<int, int>> elements;
+    elements.reserve(n);
+    for (int i = 0; i < n; ++i) {
+        elements.emplace_back(rand(), rand());
+    }
+    vector<std::pair<int, int>> backup(elements);
 
     clock_t begin = clock();
+    immutable_hash_table<int, int> ht(elements);
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout<< "Construction time: " << elapsed_secs << " seconds" << endl;
+
+    begin = clock();
     for (auto e : backup) {
         auto p = ht.find(e.first);
         assert(p->second == e.second);
     }
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout<< "Elapsed time: " << elapsed_secs << " seconds" << endl;
+    end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout<< "Searching time: " << elapsed_secs << " seconds" << endl;
+}
+
+int main() {
+    test();
     return 0;
 }
